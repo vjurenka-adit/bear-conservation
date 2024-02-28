2b1da2b80a6e2058428c02d0a1e1dc84ca9763aa
Chouffe <arthur@caillau.me>
2024-02-28 21:22:47 +0100

    [bearidentification][metriclearning] add precision_at_k

--- src/bearidentification/metriclearning/train.py
+++ src/bearidentification/metriclearning/train.py
@@ -16,7 +16,9 @@ import umap
 from cycler import cycler
 from PIL import Image
 from pytorch_metric_learning import losses, miners, samplers, testers, trainers
-from pytorch_metric_learning.utils.accuracy_calculator import AccuracyCalculator
+from pytorch_metric_learning.utils.accuracy_calculator import (
+    AccuracyCalculator,
+)  # precision_at_k,
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.transforms import v2
@@ -485,10 +487,54 @@ def save_sample_batch(dataloader: DataLoader, to: Path) -> None:
     plt.close()
 
 
+def precision_at_k(knn_labels, query_labels, k: int):
+    """Returns precision at k given the knn_labels and query_labels provided by
+    AccuracyCalculator."""
+
+    def label_comparison_fn(gt_labels, current_knn_labels):
+        t = torch.any(current_knn_labels == gt_labels, dim=1)
+        return t[:, None]
+
+    curr_knn_labels = knn_labels[:, :k]
+    same_label = label_comparison_fn(query_labels[:, None], curr_knn_labels)
+    return torch.sum(same_label).type(torch.float64) / len(same_label)
+
+
+class BearAccuracyCalculator(AccuracyCalculator):
+    def calculate_precision_at_3(self, knn_labels, query_labels, **kwargs):
+        return precision_at_k(
+            knn_labels=knn_labels,
+            query_labels=query_labels,
+            k=3,
+        )
+
+    def calculate_precision_at_5(self, knn_labels, query_labels, **kwargs):
+        return precision_at_k(
+            knn_labels=knn_labels,
+            query_labels=query_labels,
+            k=5,
+        )
+
+    def calculate_precision_at_10(self, knn_labels, query_labels, **kwargs):
+        return precision_at_k(
+            knn_labels=knn_labels,
+            query_labels=query_labels,
+            k=10,
+        )
+
+    def requires_knn(self):
+        return super().requires_knn() + [
+            "precision_at_3",
+            "precision_at_5",
+            "precision_at_10",
+        ]
+
+
 def make_accuracy_calculator() -> AccuracyCalculator:
     """Returns an accuracy calculator used to evaluate the performance of the
     model."""
-    return AccuracyCalculator(k="max_bin_count")
+    return BearAccuracyCalculator(k="max_bin_count")
+    # return AccuracyCalculator(k="max_bin_count")
 
 
 def fix_random_seed(random_seed: int = 0) -> None:
